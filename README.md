7f492513e11bae302e1d033f0264c6ff35370275
Antonio Alves <antoniocunha9610@gmail.com>
2022-08-22 22:13:54 -0300

    iniciando projeto spring

---
+++ README.md
@@ -0,0 +1,6 @@
+
+mvn spring-boot:start # rodando com o maven instalado no computaodr
+
+./mvnw spring-boot:start # roda com unix e o binário do maven na pasta oculta .mvn
+
+mvnw.cmd spring-boot:start  roda com MSdos(windows)e o binário do maven na pasta oculta .mvn
---
+++ src/main/java/br/com/antonio/console/ConsoleApplication.java
@@ -0,0 +1,15 @@
+package br.com.antonio.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Tá funcionando");
+		//SpringApplication.run(ConsoleApplication.class, args);
+	}
+
+}
